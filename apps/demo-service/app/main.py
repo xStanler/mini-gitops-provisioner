22337038da1bce9216676d18b0b307e0cde0f049
schmiele <stanislaw.chmielewski@cern.ch>
2026-07-07 16:43:51 +0200

    DEVOPS-2: Removed JSONResponse, improvemented environment variables and added strict typing for endpoints

--- apps/demo-service/app/main.py
+++ apps/demo-service/app/main.py
@@ -1,35 +1,30 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 import os
+import socket
 
 APP_VERSION = os.getenv("APP_VERSION", "local")
 COMMIT_SHA = os.getenv("COMMIT_SHA", "local")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+HOSTNAME = socket.gethostname()
 
 app = FastAPI()
 
 @app.get("/")
-async def get_root():
-    return JSONResponse(content={
+async def get_root() -> dict:
+    return {
         "name": "demo-service",
         "version": APP_VERSION,
         "commit_sha": COMMIT_SHA,
         "environment": ENVIRONMENT,
-        "hostname": "..."
-        }, status_code=200)
+        "hostname": HOSTNAME
+        }
 
 @app.get("/health")
-async def check_health():
-    return JSONResponse(
-            status_code=200,
-            content={
-                "status": "ok"
-                }
-            )
+async def check_health() -> dict:
+    return {
+            "status": "ok"
+            }
 
 @app.get("/ready")
-async def get_ready():
-    return JSONResponse(
-            status_code=200,
-            content={"status": "ready"}
-            )
+async def get_ready() -> dict:
+    return {"status": "ready"}
